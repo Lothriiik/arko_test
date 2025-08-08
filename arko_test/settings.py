50715d20525977b0ab5f835a3cd793530bb5e43e
Lothriiik <uanderson.silva@arapiraca.ufal.br>
2025-08-08 17:44:18 -0300

    fix: settings .env

--- arko_test/settings.py
+++ arko_test/settings.py
@@ -30,7 +30,7 @@ TEMPLATES = [
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
